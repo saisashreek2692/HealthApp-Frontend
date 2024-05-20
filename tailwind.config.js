14689cedaa38c3d74d6aea9eb9b670c033e40ef6
saisashreek <saisashreekd@gmail.com>
2024-05-20 16:41:20 +0530

    Added new plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     fontFamily: {
-      'Mulish': ["Mulish", "sans-serif"]
-    }
+      Mulish: ["Mulish", "sans-serif"],
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+};
