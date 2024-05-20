1eeade9cb78f0940a3a9c6058c677884504d9e85
saisashreek <saisashreekd@gmail.com>
2024-05-20 16:39:52 +0530

    App file Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 const App = () => {
   return (
     <>
-      <h1>Hello World</h1>
-      <h2>Welcome To Health Application</h2>
+      <h1 className="font-Mulish text-3xl text-black text-center p-6">Hello World</h1>
+      <h2 className="font-Mulish text-2xl text-black text-center p-3 my-3">Welcome To Health Application</h2>
     </>
   );
 };
