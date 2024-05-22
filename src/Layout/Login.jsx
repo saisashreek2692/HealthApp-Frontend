acff8898923c0a14565004a98a6d48d57285936e
saisashreek <saisashreekd@gmail.com>
2024-05-22 11:26:39 +0530

    Login Page

---
+++ src/Layout/Login.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { LoginPage } from "../Pages/index"
+const Login = () => {
+  return (
+    <section className="bg-white">
+      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+        <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
+          <img
+            alt=""
+            src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            className="absolute inset-0 h-full w-full object-cover opacity-80"
+          />
+        </aside>
+        <LoginPage />
+      </div>
+    </section>
+  );
+};
+
+export default Login;
