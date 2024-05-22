f2f3bd375c834f45cb53e8dca6e13c0de8e1f5bf
saisashreek <saisashreekd@gmail.com>
2024-05-22 11:25:37 +0530

    Added New Route for Patient

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Login from "./Layout/Login";
+import Register from "./Layout/Register";
+import Patient from "./Layout/Patient";
+
 const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />
+    },
+    {
+      path: '/register',
+      element: <Register />
+    },
+    {
+      path: '/dashboard/patient',
+      element: <Patient />
+    }
+  ]);
+
   return (
-    <>
-      <h1 className="font-Mulish text-3xl text-black text-center p-6">Hello World</h1>
-      <h2 className="font-Mulish text-2xl text-black text-center p-3 my-3">Welcome To Health Application</h2>
-    </>
+    <RouterProvider router={router} />
   );
 };
 
