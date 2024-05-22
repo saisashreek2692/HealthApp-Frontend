25fd1fbc7b967eb967932a581549e33edeb3bf23
saisashreek <saisashreekd@gmail.com>
2024-05-22 15:18:13 +0530

    Components added as Object

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,3 +1,6 @@
 export {default as LoginPage} from "./Login/Login";
 export { default as RegisterPage } from "./Register/Register";
-export { default as PatientNav } from "./Patient/PatientNav"
+export { default as PatientNav } from "./Patient/PatientNav";
+export { default as PatientHeader } from "./Patient/PatientHeader";
+export { default as PatientContent } from "./Patient/PatientContent";
+export { default as PatientFooter } from "./Patient/PatientFooter";
